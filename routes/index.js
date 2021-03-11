da6128af7547b91fde8d2e958fbccd16f0cb5553
arsine0 <raphael.caballegan@gmail.com>
2021-03-11 10:04:40 +0800

    added query params

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,14 @@ var router = express.Router();
 
 router.get('/:context', (req, res) => {
     var context = req.params.context;
-    var url = 'https://docs.erpnext.com/docs/user/manual/en';
+    var url = 'https://docs.erpnext.com/docs/user/manual/en/' + context;
 
     rp(url)
     .then(html => {
         res.send(html);
     })
     .catch(err => {
-        console.log(err);
+        console.log('err!');
     });
 });
 
