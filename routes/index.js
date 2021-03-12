52b0c57efd426e5cc0ca1094b9dd12f750fa0a92
arsine0 <raphael.caballegan@gmail.com>
2021-03-12 10:30:55 +0800

    api re-logic

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const rp = require('request-promise');
+const cheerio = require('cheerio');
+const request = require('request');
 
 var router = express.Router();
 
@@ -8,13 +10,33 @@ router.get('/:context', (req, res) => {
     var context = req.params.context;
     var url = 'https://docs.erpnext.com/docs/user/manual/en/' + context;
 
-    rp(url)
-    .then(html => {
-        res.send(html);
+    var ch_url = {
+        uri: url,
+        transform: (body) => {
+            return cheerio.load(body);
+        }
+    }
+
+    // rp(ch_url)
+    // .then($ => {
+    //     $('body').find('main').detach().appendTo('body');
+    //     $('body').children(':not(main)').remove();
+    //     res.send($.html());
+    // })
+    // .catch(err => {
+    //     console.log('err: ' + err);
+    // });
+
+    request({
+        method: 'GET',
+        url: url,
+    }, (err, resp, body) => {
+        $ = cheerio.load(body);
+        $('main').appendTo('body');
+        $('body').children(':not(main)').remove();
+
+        res.send($('body').html());
     })
-    .catch(err => {
-        console.log('err!');
-    });
 });
 
 module.exports = router;
