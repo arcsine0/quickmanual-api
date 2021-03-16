018c169152a402f47629e4f65a28823b8b7992d7
arsine0 <raphael.caballegan@gmail.com>
2021-03-16 15:08:23 +0800

    search algo v2 test

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,44 @@
 const express = require('express');
 const path = require('path');
-const rp = require('request-promise');
 const cheerio = require('cheerio');
 const request = require('request');
+const puppeteer = require('puppeteer');
 
 var router = express.Router();
 
-router.get('/:context/:contextA', (req, res) => {
-    var context = req.params.context;
-    var contextA;
-    if (req.params.contextA === 'def') { contextA = ''; }
-    else { contextA = req.params.contextA; }
-    // var url = 'https://docs.erpnext.com/docs/user/manual/en/' + context + '/' + contextA;
-    var url = 'https://docs.erpnext.com/docs/user/manual/en';   // test url
+router.get('/:context', (req, res) => {
+    async function main() {
+        var context = req.params.context;
 
-    var ch_url = {
-        uri: url,
-        transform: (body) => {
-            return cheerio.load(body);
-        }
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+
+        await page.goto('https://docs.erpnext.com/docs/user/manual/en', {waitUntil: 'networkidle2'});
+        await page.$eval('input[class="form-control"]', (el, context) => el.value = context, context);
+        await (await page.$('input[class="form-control"]')).click();
+        await page.waitForSelector('.dropdown-item');
+
+        const initContent = await page.content();
+        $ = cheerio.load(initContent);
+        $('.doc-search-container').appendTo('body');
+        $('body').children(':not(.doc-search-container)').remove();
+
+        var targetContext = $('.dropdown-menu').children('.dropdown-item').first().attr('href');
+        var targetUrl = 'https://docs.erpnext.com' + targetContext;
+        browser.close();
+
+        request({
+            method: 'GET',
+            url: targetUrl,
+        }, (err, resp, body) => {
+            $ = cheerio.load(body);
+            $('main').appendTo('body');
+            $('body').children(':not(main)').remove();
+
+            res.send($('body').html());
+        })
     }
-
-    request({
-        method: 'GET',
-        url: url,
-    }, (err, resp, body) => {
-        $ = cheerio.load(body);
-        // $('main').appendTo('body');
-        // $('body').children(':not(main)').remove();
-        $('#search-container').appendTo('body');
-        $('body').children(':not(#search-container)').remove();
-
-        res.send($('body').html());
-    })
+    main();
 });
 
 module.exports = router;
